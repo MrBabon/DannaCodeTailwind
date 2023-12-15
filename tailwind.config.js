fb5312ba261be366bd93abf1a3f5896b577c9732
MrBabon <christophedanna06@gmail.com>
2023-12-15 23:20:41 +0100

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navDanna': '#051a24',
+        'bodyDanna': '#06373A',
+        'fontDanna': '#1F5F5B',
+        'font2Danna': '#159947',
+        'flashDanna': '#4FF57B',
+      },
+      gridTemplateColumns:{
+        "menu": "175px 1fr 175px"
+      },
+    },
   },
   plugins: [],
 }
