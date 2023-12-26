543259080ae1d85a637650cdaa5918ec8a9814c0
MrBabon <christophedanna06@gmail.com>
2023-12-26 23:18:30 +0100

    deroulement fait

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,11 @@ module.exports = {
         'flashDanna': '#4FF57B',
       },
       gridTemplateColumns:{
-        "menu": "150px 1fr 175px"
+        "menu": "150px 1fr 295px"
       },
       backgroundImage: {
         'banner-home': "linear-gradient(rgba(0,0,0,0.65),rgba(0,0,0,0.65)),url('https://res.cloudinary.com/dilp6xqmb/image/upload/v1701768624/Banner_home.jpg')",
-        'valeur-home': "linear-gradient(rgba(0,0,0,0.65),rgba(0,0,0,0.65)),url('https://res.cloudinary.com/dilp6xqmb/image/upload/v1701768622/pion.jpg')",
+        'valeur-home': "linear-gradient(rgba(0,0,0,0.85),rgba(0,0,0,0.85)),url('https://res.cloudinary.com/dilp6xqmb/image/upload/v1701768622/pion.jpg')",
         'banner-service': "linear-gradient(rgba(0,0,0,0.65),rgba(0,0,0,0.65)),url('https://res.cloudinary.com/dilp6xqmb/image/upload/v1701770585/banner-service.jpg')",
       },
       textShadow: {
